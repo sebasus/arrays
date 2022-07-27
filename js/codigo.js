2fa77e13a239f8426f76c8f733634429dcd6a4e3
sebasus <susperregui.s@hotmail.com>
2022-07-26 21:43:49 -0300

    arrays

---
+++ js/codigo.js
@@ -0,0 +1,5 @@
+const peliculas = ["Minions: Nace un villano.","Thor: Love and Thunder.","Lightyear.","Jurassic World: Dominion.","Top Gun: Maverick."]
+peliculas.unshift("Elvis")
+for (let i = 0; i< peliculas.length; i++) {
+    console.log(peliculas[i])
+}
